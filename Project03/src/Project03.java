ad82f5f08846c31c3e1db956cb21db32280fb0c6
jasminebonitz <jzzbntz@gmail.com>
2025-04-07 20:48:06 -0600

    Revision
    Changed the array of objects into a list

--- Project03/src/Project03.java
+++ Project03/src/Project03.java
@@ -1,24 +1,35 @@
 /*
  * @author Markus Gulla
  * 
+ * @author Revised by Jasmine Bonitz
  */
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Project03 {
     public static void main(String[] args) throws Exception {
         Experiment[] n = new Experiment[12];
 
-        n[0] = new Experiment("Cloud Patterns", 36, 5);
-        n[1] = new Experiment("Solar Flares", 264, 9);
-        n[2] = new Experiment("Solar Power", 188, 6);
-        n[3] = new Experiment("Binary Stars", 203, 8);
-        n[4] = new Experiment("Relativity", 104, 8);
-        n[5] = new Experiment("Seed Viability", 7, 4);
-        n[6] = new Experiment("Sun Spots", 90, 2);
-        n[7] = new Experiment("Mice Tumors", 65, 8);
-        n[8] = new Experiment("Microgravity Plant Growth", 75, 5);
-        n[9] = new Experiment("Micrometeorites", 170, 9);
-        n[10] = new Experiment("Cosmic Rays", 80, 7);
-        n[11] = new Experiment("Yeast Fermentation", 27, 4);
+        List<Experiment> experiments = new ArrayList<>();
+        experiments.add(new Experiment("Cloud Patterns", 36, 5));
+        experiments.add(new Experiment("Solar Flares", 264, 9));
+        experiments.add(new Experiment("Solar Power", 188, 6));
+        experiments.add(new Experiment("Binary Stars", 203, 8));
+        experiments.add(new Experiment("Relativity", 104, 8));
+        experiments.add(new Experiment("Seed Viability", 7, 4));
+        experiments.add(new Experiment("Sun Spots", 90, 2));
+        experiments.add(new Experiment("Mice Tumors", 65, 8));
+        experiments.add(new Experiment("Microgravity Plant Growth", 75, 5));
+        experiments.add(new Experiment("Micrometeorites", 170, 9));
+        experiments.add(new Experiment("Cosmic Rays", 80, 7));
+        experiments.add(new Experiment("Yeast Fermentation", 27, 4));
 
+
+        System.out.println("Before sorting: " + experiments);
+
+        //Collections.sort(experiments, new RatingComparator());
+
+        System.out.println("After sorting by age: " + experiments);
     }
 }
