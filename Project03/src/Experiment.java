ac17b7084f42d844e527d50a04d738baa6008143
jasminebonitz <jzzbntz@gmail.com>
2025-04-09 20:19:32 -0600

    Revision

--- Project03/src/Experiment.java
+++ Project03/src/Experiment.java
@@ -1,20 +1,55 @@
 /*
  * @author Markus Gulla
- * 
- * 
+ * @author Jasmine Bonitz
  */
+public class Experiment implements Comparable<Experiment> {
+	private String name;
+	private int weight;
+	private int rating;
+	private int ratio;
+	
+	public Experiment(String name, int weight, int rating) {
+		this.name = name;
+		this.weight = weight;
+		this.rating = rating;
+		this.ratio = weight / rating;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-public class Experiment {
-String name;
-int weight;
-int rating;
-int ratio;
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
 
-// initializing
-    public Experiment(String n, int w, int r){
-        name = n;
-        weight = w;
-        rating = r;
-        ratio = w/r;
-    }
+	/**
+	 * @return the rating
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * @return the ratio
+	 */
+	public int getRatio() {
+		return ratio;
+	}
+
+	@Override
+	public String toString() {
+		return "Experiment: " + name + ", Weight: " + weight + ", Rating: " + rating + "]";
+	}
+
+	@Override
+	public int compareTo(Experiment o) {
+		return Integer.compare(this.weight, o.weight);
+	}
 }
--- Project03/src/Project03.java
+++ Project03/src/Project03.java
@@ -1,41 +1,47 @@
-
+import java.util.Arrays;
 
 /*
  * @author Markus Gulla
- * 
- * @author Revised by Jasmine Bonitz
+ * @author Jasmine Bonitz
  */
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Project03 {
     public static void main(String[] args) throws Exception {
         Experiment[] n = new Experiment[12];
         Cargo c = new Cargo();
 
-        List<Experiment> experiments = new ArrayList<>();
-        experiments.add(new Experiment("Cloud Patterns", 36, 5));
-        experiments.add(new Experiment("Solar Flares", 264, 9));
-        experiments.add(new Experiment("Solar Power", 188, 6));
-        experiments.add(new Experiment("Binary Stars", 203, 8));
-        experiments.add(new Experiment("Relativity", 104, 8));
-        experiments.add(new Experiment("Seed Viability", 7, 4));
-        experiments.add(new Experiment("Sun Spots", 90, 2));
-        experiments.add(new Experiment("Mice Tumors", 65, 8));
-        experiments.add(new Experiment("Microgravity Plant Growth", 75, 5));
-        experiments.add(new Experiment("Micrometeorites", 170, 9));
-        experiments.add(new Experiment("Cosmic Rays", 80, 7));
-        experiments.add(new Experiment("Yeast Fermentation", 27, 4));
+        Experiment experiments[] = {
+		        new Experiment("Cloud Patterns", 36, 5),
+		        new Experiment("Solar Flares", 264, 9),
+		        new Experiment("Solar Power", 188, 6),
+		        new Experiment("Binary Stars", 203, 8),
+		        new Experiment("Relativity", 104, 8),
+		        new Experiment("Seed Viability", 7, 4),
+		        new Experiment("Sun Spots", 90, 2),
+		        new Experiment("Mice Tumors", 65, 8),
+		        new Experiment("Microgravity Plant Growth", 75, 5),
+		        new Experiment("Micrometeorites", 170, 9),
+		        new Experiment("Cosmic Rays", 80, 7),
+		        new Experiment("Yeast Fermentation", 27, 4)
+        };
 
+        System.out.println("Before sorting:");
+        for (Experiment e : experiments) {
+        	System.out.println(e);
+        }
+        System.out.println();
+        
+        Arrays.sort(experiments);
 
-        System.out.println("Before sorting: " + experiments);
-
-        //Collections.sort(experiments, new RatingComparator());
-
+        System.out.println("After sorting:");
+        for (Experiment e : experiments) {
+        	System.out.println(e);
+        }
+        
+        /*
         System.out.println("After sorting by age: " + experiments);
         for (int i = 0; i < n.length; i++){
             System.out.print(c.add(n[i]));
         }
+        */
     }
 }
